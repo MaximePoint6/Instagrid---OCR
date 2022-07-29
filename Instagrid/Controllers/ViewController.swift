fc845907ff60538dff51155e24ff266d57533b96
Maxime Point <maximepoint6@gmail.com>
2022-07-29 14:46:06 +0200

    code improvement - functional app

--- Instagrid/Controllers/ViewController.swift
+++ Instagrid/Controllers/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import AVFoundation
 import Photos
+import PhotosUI
 
 class ViewController: UIViewController {
     
@@ -18,6 +19,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentLayoutView: LayoutView!
     var buttonClicked: UIButton!
     var orientation: Orientation = .portrait
+    let negativeTranslationToShare: CGFloat = -150
+    var imageToShare = false
     
     enum Orientation {
         case landscape
@@ -27,7 +30,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        
         // Initialize Swipe Gesture Recognizer
         let swipeGestureRecognizerUp = UIPanGestureRecognizer(target: self, action: #selector(didSwipe(_:)))
         currentLayoutView.addGestureRecognizer(swipeGestureRecognizerUp)
@@ -36,13 +38,7 @@ class ViewController: UIViewController {
     //MARK: DEvice orientation
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        if UIDevice.current.orientation.isLandscape {
-            print("Landscape")
-            orientation = .landscape
-        } else {
-            print("Portrait")
-            orientation = .portrait
-        }
+        orientation = UIDevice.current.orientation.isLandscape ? .landscape : .portrait
     }
     
     private func setupUI() {
@@ -52,9 +48,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func layoutSelection(_ sender: UIButton) {
-        for layout in self.layouts {
-            layout.isSelected = false
-        }
+        _ = self.layouts.map { $0.isSelected = false } // Remplace boucle for
         sender.isSelected = !sender.isSelected
         sender.setImage(UIImage(named: "Selected"), for: .selected)
         sender.contentVerticalAlignment = .fill
@@ -80,15 +74,13 @@ class ViewController: UIViewController {
         case .began, .changed:
             transformeView(gesture: sender)
         case .ended, .cancelled:
-            let translation = sender.translation(in: currentLayoutView)
-            if translation.y > -100 || translation.x > -100 {
-                resetLayoutView()
-            }
+            animationView()
         default:
             break
         }
     }
     
+    //MARK: Animations
     private func transformeView(gesture: UIPanGestureRecognizer) {
         // Translation
         var translation = gesture.translation(in: currentLayoutView)
@@ -96,79 +88,74 @@ class ViewController: UIViewController {
         case .portrait:
             if translation.y >= 0 {
                 translation.y = 0
-            } else if translation.y < -100 {
-                animationView()
-                picturesharing()
+            } else if translation.y <= negativeTranslationToShare {
+                imageToShare = true
             } else {
                 let translationTransform = CGAffineTransform(translationX: 0, y: translation.y)
                 currentLayoutView.transform = translationTransform
+                imageToShare = false
             }
         case .landscape:
             if translation.x >= 0 {
                 translation.x = 0
-            } else if translation.x < -100 {
-                animationView()
-                picturesharing()
+            } else if translation.x <= negativeTranslationToShare {
+                imageToShare = true
             } else {
                 let translationTransform = CGAffineTransform(translationX: translation.x, y: 0)
                 currentLayoutView.transform = translationTransform
+                imageToShare = false
             }
         }
     }
     
     private func animationView() {
         // Animation Question View
-        let screenHeight = UIScreen.main.bounds.height
-        let screenWidth = UIScreen.main.bounds.width
-        let translationTransform: CGAffineTransform
-        switch orientation {
-        case .landscape:
-            translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
-        case .portrait:
-            translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+        if imageToShare == false {
+            resetLayoutView()
+        } else {
+            let screenHeight = UIScreen.main.bounds.height
+            let screenWidth = UIScreen.main.bounds.width
+            let translationTransform: CGAffineTransform
+            switch orientation {
+            case .landscape:
+                translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
+            case .portrait:
+                translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+            }
+            UIView.animate(withDuration: 0.5, animations: {
+                self.currentLayoutView.transform = translationTransform
+            }, completion: { (success) in
+                if success {
+                    self.picturesharing()
+                }
+            })
         }
-        UIView.animate(withDuration: 0.3, animations: {
-          self.currentLayoutView.transform = translationTransform
-        }, completion: { (success) in
-          if success {
-            self.resetLayoutView()
-          }
-        })
     }
     
     private func resetLayoutView() {
-        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-          self.currentLayoutView.transform = .identity
+        UIView.animate(withDuration: 0.5, animations: {
+            self.currentLayoutView.transform = .identity
         }, completion:nil)
-      }
+    }
     
     //MARK: picture sharing
     private func picturesharing(){
         let items = [self.currentLayoutView.asImage()]
         let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
-        present(ac, animated: true)
+        present(ac, animated: true, completion: nil)
+        imageToShare = false
+        resetLayoutView()
     }
     
     //MARK: ImagePicker
     private func showImagePickerOption() {
         let alertVC = UIAlertController(title: "Pick a Photo", message: "Choose a picture from Library or camera", preferredStyle: .actionSheet)
-        
-        // Image Picker for camera
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { [weak self] (action) in
-            guard let self = self else {
-                return
-            }
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
             self.cameraAuthorization()
         }
-        
-        // Image Picker for library
-        let libraryAction = UIAlertAction(title: "Library", style: .default) { [weak self] (action) in
-            guard let self = self else {
-                return
-            }
+        let libraryAction = UIAlertAction(title: "Library", style: .default) { _ in
             self.photosAutorization()
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         alertVC.addAction(cameraAction)
@@ -183,19 +170,20 @@ class ViewController: UIViewController {
         case .authorized: // The user has previously granted access to the camera.
             let cameraImagePicker = self.imagePicker(sourceType: .camera)
             cameraImagePicker.delegate = self
-            present(cameraImagePicker, animated: true){
-            }
+            present(cameraImagePicker, animated: true){}
         case .notDetermined: // The user has not yet been asked for camera access.
             AVCaptureDevice.requestAccess(for: .video) { granted in
                 if granted {
-                    let cameraImagePicker = self.imagePicker(sourceType: .camera)
-                    cameraImagePicker.delegate = self
-                    self.present(cameraImagePicker, animated: true){}
+                    DispatchQueue.main.async {
+                        let cameraImagePicker = self.imagePicker(sourceType: .camera)
+                        cameraImagePicker.delegate = self
+                        self.present(cameraImagePicker, animated: true){}
+                    }
                 }
             }
         case .denied: // The user has previously denied access.
             let alert = UIAlertController(title: "Setting", message: "This app is not authorized to use Camera. Please allow the app to use the camera in settings.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
+            alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { _ in
                 DispatchQueue.main.async {
                     if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
                         UIApplication.shared.open(settingsURL)
@@ -213,32 +201,47 @@ class ViewController: UIViewController {
     
     private func photosAutorization(){
         // Authorization for Photos
-        let photoAuthorization = PHPhotoLibrary.authorizationStatus()
-        switch photoAuthorization {
-        case .authorized, .limited: // The user has previously granted access to the photo.
-            let cameraImagePicker = self.imagePicker(sourceType: .photoLibrary)
-            cameraImagePicker.delegate = self
-            present(cameraImagePicker, animated: true){
-            }
+        var authorizationStatus : PHAuthorizationStatus
+        if #available(iOS 14, *) {
+            authorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        } else {
+            authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        }
+        switch authorizationStatus {
+        case .authorized: // The user has previously granted access to the photo.
+            let imagePicker = self.imagePicker(sourceType: .photoLibrary)
+            imagePicker.delegate = self
+            present(imagePicker, animated: true){}
+        case .limited:
+            let imagePicker = self.imagePicker(sourceType: .photoLibrary)
+            imagePicker.delegate = self
+            present(imagePicker, animated: true){}
         case .notDetermined: // The user has not yet been asked for photos access.
-            PHPhotoLibrary.requestAuthorization({status in
-                if #available(iOS 14, *) {
-                    if status == .authorized || status == .limited {
-                        let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
-                        libraryImagePicker.delegate = self
-                        self.present(libraryImagePicker, animated: true){}
+            if #available(iOS 14, *) {
+                PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                    DispatchQueue.main.async{
+                        if status == .authorized {
+                            let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
+                            libraryImagePicker.delegate = self
+                            self.present(libraryImagePicker, animated: true){}
+                        } else if status == .limited {
+                            let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
+                            libraryImagePicker.delegate = self
+                            self.present(libraryImagePicker, animated: true){}
+                        }
                     }
-                } else {
+                }
+            } else {
+                PHPhotoLibrary.requestAuthorization(){ status in
                     if status == .authorized {
                         let libraryImagePicker = self.imagePicker(sourceType: .photoLibrary)
                         libraryImagePicker.delegate = self
                         self.present(libraryImagePicker, animated: true){}
                     }
                 }
-            })
+            }
         case .denied: // The user has previously denied access.
             let alert = UIAlertController(title: "Setting", message: "This app is not authorized to use Photos. Please allow the app to use photos in settings.", preferredStyle: .alert)
-            
             alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
                 DispatchQueue.main.async {
                     if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
@@ -264,7 +267,6 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.originalImage] as! UIImage
         self.buttonClicked.setImage(image, for: .normal)
